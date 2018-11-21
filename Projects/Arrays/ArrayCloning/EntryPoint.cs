9f6d544ebd5c4dfea5936fee3e9ed4b4f0431775
Jerry Hampton <hamptonjt@hotmail.com>
2018-11-20 20:08:38 -0500

    #100DaysOfCode - D14 - Cloning/Copying and Reversing Arrays

---
+++ Projects/Arrays/ArrayCloning/EntryPoint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        //int[] secondArray = (int[])array.Clone();  // creates new values for the 2nd array...
+        int[] secondArray = new int[array.Length];
+
+        Array.Copy(array, secondArray, array.Length);  // more versatile...
+
+        array[2] = 10;
+
+        Console.WriteLine(array[2]);
+        Console.WriteLine(secondArray[2]);
+
+        int[] testArr = { 1, 2, 3, 4 };
+        int[] secTestArr = new int[testArr.Length];
+
+        IntArrayCopy(testArr, secTestArr, testArr.Length);
+        for (int i = 0; i < testArr.Length; i++)
+        {
+            Console.WriteLine($"source = {testArr[i]}");
+            Console.WriteLine($"copy = {secTestArr[i]}");
+        }
+    }
+
+    // challenge
+    static void IntArrayCopy(int[] a1, int[] a2, int numValsToCopy)
+    {
+        if (a1.Length == 0 || numValsToCopy > a1.Length)
+        {
+            throw new Exception("Invalid Array Length");
+        }
+
+        for (int i = 0; i < a1.Length; i++)
+        {
+            if (i == numValsToCopy)
+            {
+                break;
+            }
+            else
+            {
+                a2[i] = a1[i];
+            }
+        }
+    }
+}
---
+++ Projects/Arrays/Reversing/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string[] fruits = { "orange", "lemon", "strawberry", "apple", "banana" };
+
+        //Array.Reverse(fruits);  // overwrites the original array...
+
+        //foreach (var fruit in fruits)
+        //{
+        //    Console.WriteLine(fruit);
+        //}
+
+        MyReverse(fruits, 1, 4);
+
+        foreach (var fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+    }
+
+    // challenge
+    static void MyReverse(string[] a1, int startIndex = 0, int endIndex = 0)
+    {
+        if (endIndex == 0)
+        {
+            endIndex = a1.Length - 1;
+        }
+
+        for (int i = 0; i < a1.Length; i++)
+        {
+            if (i >= startIndex && i <= endIndex)
+            {
+                // swap the values...
+                string temp = a1[i];
+                a1[i] = a1[endIndex];
+                a1[endIndex] = temp;
+                endIndex--;
+            }
+        }
+
+    }
+}
